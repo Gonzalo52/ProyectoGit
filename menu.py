b4a0bd79c6959fe6bd9c1ea2753b67ced2db3819
Gonzalo <gtggomez1@gmail.com>
2023-01-24 11:20:38 -0300

    creacion de def elegir opcion

--- menu.py
+++ menu.py
@@ -1,4 +1,20 @@
 menu = ['Ver la lista de contactos[ver]', 'Buscar un contacto[buscar]', 'Agregar un contactos[agregar]',
  'Eliminar un contacto[eliminar]', 'Editar contacto[editar]']
 
- 
+ def elegir_opcion():
+    elige = input("\ningresa una opcion: ")
+    while elige == "ver" or "buscar" or "agregar" or "eliminar" or "editar":
+        if elige == "ver":
+            return ver_Contactos()
+        if elige == "buscar":
+            return buscar_Contactos()
+        if elige == "agregar":
+            return agregar_Contactos()
+        if elige == "eliminar":
+            return eliminar_Contacto()
+        if elige == "editar":
+            return editar_Contacto()
+        else:
+            print("\nno eligio una opcion correcta\n")
+            return llamar_menu()
+
