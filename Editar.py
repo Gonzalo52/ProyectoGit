3f7b6ab8925ef2e1e3ce3dca546d46358c136131
Gonzalo <gtggomez1@gmail.com>
2023-01-24 12:15:42 -0300

    agregar que se edita en def editar

--- Editar.py
+++ Editar.py
@@ -4,3 +4,26 @@ def editar_Contacto():
     esta = False
     for x in contactos:
         if nombre == x['nombre'] and apellido == x['apellido']:
+            elige = input("\nque quieres editar? \nnombre apellido numero :")
+            while elige == "nombre" or "apellido" or "numero":
+                if elige == "nombre":
+                    nombre1 = input("ingresa el nombre del contacto: ")
+                    x['nombre'] = nombre1
+                    print("\nel contacto se edito correctamente\n")
+                    esta = True
+                    return llamar_menu()
+                if elige == "apellido": 
+                    apellido1 = input("ingresa el apellido del contacto: ")   
+                    x['apellido'] = apellido1
+                    print("\nel contacto se edito correctamente\n")
+                    esta = True
+                    return llamar_menu()
+                if elige == "numero":
+                    numero1 = input("ingresa el numero correcto: ")
+                    x['numero'] = numero1
+                    print("\nel contacto se edito correctamente\n")
+                    esta = True
+                    return llamar_menu()    
+                else:
+                    print("seleccione una opcion correcta")
+                    elige = input("\nque quieres editar? \nnombre apellido numero :")
