b57f2053292ffad03a8ab04cbddad3c6ebe988ac
Gonzalo <gtggomez1@gmail.com>
2023-01-24 12:04:53 -0300

    funcion eliminar contactos

---
+++ eliminar.py
@@ -0,0 +1,13 @@
+def eliminar_Contacto():
+    nombre = input("ingresa el nombre del contacto: ")
+    apellido = input("ingresa el apellido del contacto: ")
+    esta = False
+    for i in contactos:
+        if nombre == i['nombre'] and apellido == i['apellido']:
+            contactos.remove(i)
+            print("\nel contacto se elimino correctamente\n")
+            esta = True
+            return llamar_menu()
+    if esta == False:
+            print("\nel contacto no existe")
+            return llamar_menu()
